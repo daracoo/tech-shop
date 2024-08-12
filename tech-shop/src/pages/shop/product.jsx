7c429adf303e6c86717b95d820a5e94b480c5bd2
daracoo <dkuzmanoski21@gmail.com>
2024-08-12 10:33:16 +0200

    Added shop context

--- tech-shop/src/pages/shop/product.jsx
+++ tech-shop/src/pages/shop/product.jsx
@@ -1,11 +1,29 @@
+import PropTypes from 'prop-types';
+import { ShopContext } from '../../context/shop-context';
+import { useContext } from 'react';
+
 export const Product = (props) => {
     const {id, productName, price, productImage} = props.data
+    const {addToCart, cartItems} = useContext(ShopContext)
+    const cartItemAmount = cartItems[id]
+
     return <div className="product">
         <img src={productImage} />
         <div className="description">
             <p><b>{productName}</b></p>
             <p>${price}</p>
         </div>
-        <button className="addToCartBttn">Add To Cart</button>
+        <button className="addToCartBttn" onClick={() => addToCart(id)}>
+            Add To Cart {cartItemAmount > 0 && <>({cartItemAmount})</>}
+        </button>
     </div>
 }
+
+Product.propTypes = {
+    data: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        productName: PropTypes.string.isRequired,
+        price: PropTypes.number.isRequired,
+        productImage: PropTypes.string.isRequired,
+    }).isRequired,
+};
