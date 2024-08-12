8584cbe6cdca6fd2cfbceec3d3ba785f65faac2b
daracoo <dkuzmanoski21@gmail.com>
2024-08-12 11:05:10 +0200

    Adding the products to cart

--- tech-shop/src/pages/cart/cart.jsx
+++ tech-shop/src/pages/cart/cart.jsx
@@ -1,4 +1,33 @@
+import {PRODUCTS} from '../../products'
+import { ShopContext } from '../../context/shop-context'
+import { useContext } from 'react'
+import { CartItem } from './cart-item'
+import './cart.css'
+import { useNavigate } from 'react-router-dom' 
+
 export const Cart = () => {
-    return <>
-    </>
+    const {cartItems, getTotalCartAmount} = useContext(ShopContext)
+    const totalAmount = getTotalCartAmount()
+
+    const navigate = useNavigate()
+    return <div className="cart">
+        <div>
+            <h1>Your Cart Items</h1>
+        </div>
+        <div className="cartItems">
+            {PRODUCTS.map((product) => {
+                if(cartItems[product.id] !== 0){
+                    return <CartItem key={product.id} data={product} />
+                }
+            })}
+        </div>
+        {totalAmount > 0 ? 
+        <div className='checkout'>
+            <p>Subtotal: ${totalAmount}</p>
+            <button onClick={() => navigate("/")}>Continue Shopping</button>
+            <button>Checkout</button>
+        </div> : 
+        <h1>Your Cart is Empty</h1>
+        }
+    </div>
 }
